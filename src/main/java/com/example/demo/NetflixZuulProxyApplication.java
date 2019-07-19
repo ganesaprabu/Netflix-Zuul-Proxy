53880896008e205d4a85230d3e8987de7d4687c6
Ganesaprabu Navamanirajan <ganesa.tech@gmail.com>
2019-07-19 19:36:14 +0530

    Added the Zipkin

--- src/main/java/com/example/demo/NetflixZuulProxyApplication.java
+++ src/main/java/com/example/demo/NetflixZuulProxyApplication.java
@@ -2,14 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class NetflixZuulProxyApplication {
 
 	public static void main(String[] args) {
